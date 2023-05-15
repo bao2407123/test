763492c7e0efb1d48682ded9a8572b9366dfd173
bao2407123 <130550003+bao2407123@users.noreply.github.com>
2023-05-15 16:27:36 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {fgw : 'Greenwich'});
 });
 
+router.get('/greenwich', (req,res) => {
+  res.render('fgw')
+})
+
+router.get('/hanoi', (req, res) =>{
+  res.render('hanoi', { hn : 'Hanoi', vn : 'Vietnam'})
+})
+
+router.get('/vietnam', (req, res) =>{
+  var country = 'Vietnam'
+  var capital = 'Hanoi'
+  var population = '90 millions'
+  var location = 'South East Asia'
+  var sports ='football'
+  var image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnBECql4xbMyFK2lUPgSNG7mTJ8HJoJV-_ZA&usqp=CAU'
+  res.render('vietnam',
+  {ten_nuoc : country, thu_do :  capital, dan_so : population,
+  vi_tri : location, anh : image, monthethaodcyeuthich: sports })
+})
 module.exports = router;
